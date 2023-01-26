1b846fd018f72a681fda463441ab2a0db50265fc
Ben Dorton <ben.dorton@hughes.com>
2023-01-26 12:58:01 -0700

    Add auth using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'template/view'
   get 'template/list'
   get 'profile/view'
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root to: "home#index"
 end
